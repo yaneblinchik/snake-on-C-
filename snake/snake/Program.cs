1cb11ad7ec0041fc37a88558f2a7159fb21fabd3
yaneblinchik <yaneblinchik@gmail.com>
2015-12-15 20:28:10 +0300

    Конструктор. Инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,11 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Point p1 = new Point(1,3,'*');
+            p1.Draw();
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
             Console.ReadLine();        
         }
     }
